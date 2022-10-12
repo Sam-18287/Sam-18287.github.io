1a6d76b13b455a48b0efef72811d8a79c3ec3ff6
Sam-18287 <sam.naserpour@changehealthcare.com>
2022-10-12 16:59:33 -0700

    -1

--- module1.js
+++ module1.js
@@ -1,4 +1,18 @@
-function myfunction()
- {
-document.write("welcome to Javatpoint");
-         }
+
+function counter()
+    {
+    document.write("welcome to Javatpoint");
+    document.write("<br>");
+    c = c + 1;
+    writer.write(c)
+    document.write(c);
+
+}
+
+
+function WriteToFile() {
+
+    document.write("Start of WriteToFile")
+    document.write("<br>");
+    document.write("End of WriteToFile")
+}
